cab04b15f4fd988f383ce67b893f1feb701fc247
Aldas <80693733+aldask@users.noreply.github.com>
2023-04-11 20:48:59 +0300

    header and first section layouts done

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div className='headerSide1'>
+          <a href='#'><img src='somerandompic.png' alt='logo'></img></a>
+        </div>
+        <div className='headerSide2'>
+          <a href='home'>Home</a>
+          <a href='home'>Home</a>
+          <a href='home'>Home</a>
+          <a href='home'>Home</a>
+        </div>
       </header>
-    </div>
+      <div className='mainContainer'>
+        <div className='firstContainer'>
+          <h1 className='welcomeHeader'>Welcome to TypeO</h1>
+          <p className='welcomeText'>Lorem ipsum dolor sit amet, 
+          consectetur adipisicing elit. Corporis nostrum deleniti asperiores 
+          impedit! Ratione libero tempore magni facilis officiis cum odit laboriosam consectetur 
+          illo accusantium iure quam maxime, non architecto!</p>
+        </div>
+      </div>
+    </>
   );
 }
 
