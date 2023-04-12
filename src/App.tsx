f02aa7951d7192efbe82fec235b2853ffd28ed3c
Aldas <80693733+aldask@users.noreply.github.com>
2023-04-12 18:49:05 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/Home';
 
 function App() {
   return (
     <>
-      <header>
-        <div className='headerSide1'>
-          <a href='#'><img src='somerandompic.png' alt='logo'></img></a>
-        </div>
-        <div className='headerSide2'>
-          <a href='home'>Home</a>
-          <a href='home'>Home</a>
-          <a href='home'>Home</a>
-          <a href='home'>Home</a>
-        </div>
-      </header>
-      <div className='mainContainer'>
-        <div className='firstContainer'>
-          <h1 className='welcomeHeader'>Welcome to TypeO</h1>
-          <p className='welcomeText'>Lorem ipsum dolor sit amet, 
-          consectetur adipisicing elit. Corporis nostrum deleniti asperiores 
-          impedit! Ratione libero tempore magni facilis officiis cum odit laboriosam consectetur 
-          illo accusantium iure quam maxime, non architecto!</p>
-        </div>
-      </div>
+      <HomePage />
     </>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+
+function HomePage() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default HomePage;
