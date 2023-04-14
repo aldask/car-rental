c51c23ecee38e9003b8665f98d8cb93d3eebd059
Aldas <80693733+aldask@users.noreply.github.com>
2023-04-15 00:07:49 +0300

    v1.6

--- src/components/Booking.tsx
+++ src/components/Booking.tsx
@@ -1,9 +1,53 @@
 function Booking() {
   return (
   <>
-    <div className="booking-Container">
-        test
-    </div>
+    <section className="booking-Container">
+        <h1 className="bookingTitle">Book a Car</h1>
+        <div className="booking-box">
+          <div className="selectionBox">
+            <label>Car Model <span className="red">*</span></label>
+            <select>
+              <option>Select car model</option>
+              <option>VW Golf GTI</option>
+              <option>Tesla Model S</option>
+              <option>Bugatti Chiron</option>
+              <option>Lamborghini Gallardo</option>
+            </select>
+          </div>
+          <div className="selectionBox">
+            <label>Pick-up location <span className="red">*</span></label>
+            <select>
+              <option>Select car pick-up location</option>
+              <option>Vilnius</option>
+              <option>Kaunas</option>
+              <option>Riga</option>
+              <option>Talin</option>
+            </select>
+          </div>
+          <div className="selectionBox">
+            <label>Drop-off location <span className="red">*</span></label>
+            <select>
+              <option>Select car drop-off location</option>
+              <option>Vilnius</option>
+              <option>Kaunas</option>
+              <option>Riga</option>
+              <option>Talin</option>
+            </select>
+          </div>
+          <div className="selectionBox">
+            <label>Car pick-up date <span className="red">*</span></label>
+            <input type="date"></input>
+          </div>
+          <div className="selectionBox">
+            <label>Car drop-off date <span className="red">*</span></label>
+            <input type="date"></input>
+          </div>
+          <div className="selectionBox">
+            <label>random shit</label>
+            <button className="confirmBooking">Confirm</button>
+          </div>
+        </div>
+    </section>
   </>
   );
 }
