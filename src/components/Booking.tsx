97a2e112db05d22bf93a6b0b015cc0296ca8f91e
Aldas <80693733+aldask@users.noreply.github.com>
2023-04-19 22:46:21 +0300

    started coding booking section logic

--- src/components/Booking.tsx
+++ src/components/Booking.tsx
@@ -1,5 +1,30 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
+
 function Booking() {
+
+  const [carModel, setCarModel] = useState("");
+  const [pickUpCity, setPickUpCity] = useState("");
+  const [dropOffCity, setDropOffCity] = useState("");
+  const [pickDate, setPickDate] = useState("");
+  const [dropDate, setDropDate] = useState("");
+
+  const handleCar = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCarModel(e.target.value);
+  };
+  const handlePickCity = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPickUpCity(e.target.value);
+  };
+  const handleDropCity = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDropOffCity(e.target.value);
+  }
+  const handlePickDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPickDate(e.target.value);
+  }
+  const handleDropDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDropDate(e.target.value);
+  }
+
   return (
   <>
     <section className="booking">
@@ -9,7 +34,7 @@ function Booking() {
           <div className="bookingBox">
             <div className="selectionBox">
               <label>Car Model <span className="red">*</span></label>
-              <select>
+              <select value={carModel} onChange={handleCar}>
                 <option>Select car model</option>
                 <option>VW Golf GTI</option>
                 <option>Tesla Model S</option>
@@ -19,7 +44,7 @@ function Booking() {
             </div>
             <div className="selectionBox">
               <label>Pick-up location <span className="red">*</span></label>
-              <select>
+              <select value={pickUpCity} onChange={handlePickCity}>
                 <option>Select car pick-up location</option>
                 <option>Vilnius</option>
                 <option>Kaunas</option>
@@ -29,7 +54,7 @@ function Booking() {
             </div>
             <div className="selectionBox">
               <label>Drop-off location <span className="red">*</span></label>
-              <select>
+              <select value={dropOffCity} onChange={handleDropCity}>
                 <option>Select car drop-off location</option>
                 <option>Vilnius</option>
                 <option>Kaunas</option>
@@ -39,11 +64,11 @@ function Booking() {
             </div>
             <div className="selectionBox">
               <label>Car pick-up date <span className="red">*</span></label>
-              <input type="date"></input>
+              <input type="date" value={pickDate} onChange={handlePickDate}></input>
             </div>
             <div className="selectionBox">
               <label>Car drop-off date <span className="red">*</span></label>
-              <input type="date"></input>
+              <input type="date" value={dropDate} onChange={handleDropDate}></input>
             </div>
             <div className="selectionBox">
               <button className="confirmBooking">Confirm</button>
