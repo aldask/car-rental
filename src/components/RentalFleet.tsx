439a6ae29c11083deb0ffc733f4765f22b0f175d
Aldas <80693733+aldask@users.noreply.github.com>
2023-05-01 15:07:22 +0300

    Fixed fleet CSS. Improved code readability

--- src/components/RentalFleet.tsx
+++ src/components/RentalFleet.tsx
@@ -1,23 +1,36 @@
 import { useState } from "react";
-import { cars } from './Booking';
+import { cars } from "./Booking";
 
 // Set car model and img
 
 function RentalFleet() {
-
   const [selectedFleetCar, setSelectedFleetCar] = useState(cars[0]);
-  const [selectedCarPrice, setSelectedCarPrice] = useState(selectedFleetCar.price);
-  const [selectedFleetCarImg, setSelectedFleetCarImg] = useState(selectedFleetCar.img);
-  const [selectedCarManufaturer, setSelectedCarManufaturer] = useState(selectedFleetCar.manufacturer);
-  const [selectedCarModel, setSelectedCarModel] = useState(selectedFleetCar.model);
+  const [selectedCarPrice, setSelectedCarPrice] = useState(
+    selectedFleetCar.price
+  );
+  const [selectedFleetCarImg, setSelectedFleetCarImg] = useState(
+    selectedFleetCar.img
+  );
+  const [selectedCarManufaturer, setSelectedCarManufaturer] = useState(
+    selectedFleetCar.manufacturer
+  );
+  const [selectedCarModel, setSelectedCarModel] = useState(
+    selectedFleetCar.model
+  );
   const [selectedCarYear, setSelectedCarYear] = useState(selectedFleetCar.year);
-  const [selectedCarDoors, setSelectedCarDoors] = useState(selectedFleetCar.doors);
+  const [selectedCarDoors, setSelectedCarDoors] = useState(
+    selectedFleetCar.doors
+  );
   const [selectedCarAC, setSelectedCarAC] = useState(selectedFleetCar.ac);
-  const [selectedCarTransmission, setSelectedCarTransmission] = useState(selectedFleetCar.transmission);
+  const [selectedCarTransmission, setSelectedCarTransmission] = useState(
+    selectedFleetCar.transmission
+  );
   const [selectedCarFuel, setSelectedCarFuel] = useState(selectedFleetCar.fuel);
 
-  const handleCarFleet = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    const selectedCar = cars.find(car => car.label === e.currentTarget.value);
+  const handleCarFleet = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    const selectedCar = cars.find((car) => car.label === e.currentTarget.value);
     if (selectedCar) {
       setSelectedFleetCar(selectedCar);
       setSelectedCarPrice(selectedCar.price);
@@ -32,60 +45,73 @@ function RentalFleet() {
     }
   };
 
-
   return (
     <>
-    <section className="fleetContainer">
-      <div className="containerTexts">
-        <h1>Our Rental Fleet</h1>
-        <p>Choose from a variety of our amazing vehicles to rent for your next adventure or business trip.</p>
-    </div>
-    <div className="carSelections">
-      <div className="carsSelect">
-        {cars.map((car) => (
-        <button key={car.label} value={car.label} onClick={handleCarFleet}>
-          {car.label}</button>
-          ))}
-      </div>
-      <div className="carPic">
-        <img src={selectedFleetCarImg} alt="chosen-car-picture"></img>
-      </div>
-      <div className="carDetails">
-        <p className="price">Price:&nbsp;<span className="strong">{selectedCarPrice}€</span>/day</p>
-        <div className="row">
-          <div className="label">Manufacturer</div>
-          <div className="value">{selectedCarManufaturer}</div>
+      <section className="fleet">
+        <div className="container">
+          <div className="fleetContainer">
+            <div className="fleetTxt">
+              <h1>Our Rental Fleet</h1>
+              <p>
+                Choose from a variety of our amazing vehicles to rent for your
+                next adventure or business trip.
+              </p>
+            </div>
+            <div className="carSelections">
+              <div className="carsSelect">
+                {cars.map((car) => (
+                  <button
+                    key={car.label}
+                    value={car.label}
+                    onClick={handleCarFleet}
+                  >
+                    {car.label}
+                  </button>
+                ))}
+              </div>
+              <div className="carPic">
+                <img src={selectedFleetCarImg} alt="chosen-car-picture"></img>
+              </div>
+              <div className="carDetails">
+                <p className="price">Price:&nbsp;<span className="strong">{selectedCarPrice}€</span>/day</p>
+                <div className="row">
+                  <div className="label">Manufacturer</div>
+                  <div className="value">{selectedCarManufaturer}</div>
+                </div>
+                <div className="row">
+                  <div className="label">Model</div>
+                  <div className="value">{selectedCarModel}</div>
+                </div>
+                <div className="row">
+                  <div className="label">Year</div>
+                  <div className="value">{selectedCarYear}</div>
+                </div>
+                <div className="row">
+                  <div className="label">Doors</div>
+                  <div className="value">{selectedCarDoors}</div>
+                </div>
+                <div className="row">
+                  <div className="label">AC</div>
+                  <div className="value">{selectedCarAC}</div>
+                </div>
+                <div className="row">
+                  <div className="label">Transmission</div>
+                  <div className="value">{selectedCarTransmission}</div>
+                </div>
+                <div className="row">
+                  <div className="label">Fuel</div>
+                  <div className="value">{selectedCarFuel}</div>
+                </div>
+                <button className="reserveButton">
+                  <a href="#book">Reserve</a>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="row">
-          <div className="label">Model</div>
-          <div className="value">{selectedCarModel}</div>
-        </div>
-        <div className="row">
-          <div className="label">Year</div>
-          <div className="value">{selectedCarYear}</div>
-        </div>
-        <div className="row">
-          <div className="label">Doors</div>
-          <div className="value">{selectedCarDoors}</div>
-        </div>
-        <div className="row">
-          <div className="label">AC</div>
-          <div className="value">{selectedCarAC}</div>
-        </div>
-        <div className="row">
-          <div className="label">Transmission</div>
-          <div className="value">{selectedCarTransmission}</div>
-        </div>
-        <div className="row">
-          <div className="label">Fuel</div>
-          <div className="value">{selectedCarFuel}</div>
-        </div>
-        <button className="reserveButton"><a href="#book">Reserve</a></button>
-      </div>
-    </div>
-  </section>
-      </>
-    );
-  }
+      </section>
+    </>
+  );
+}
 
 export default RentalFleet;
