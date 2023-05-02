d5530988ca5ae512dae9a86a177e76f887663727
Aldas <80693733+aldask@users.noreply.github.com>
2023-05-02 22:08:26 +0300

    Added dropdown animation. Need to improve. Check widths

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,13 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 interface Question {
   asking: string;
   answer: string;
 }
 
-export const questions: Question[] = [
+const questions: Question[] = [
   {
     asking: "1. What is special about comparing rental car deals?",
     answer: "Comparing rental car deals is important as it helps find the best deal that fits your budget and requirements, ensuring you get the most value for your money. By comparing various options, you can find deals that offer lower prices, additional services, or better car models. You can find car rental deals by researching online and comparing prices from different rental companies.",
@@ -23,50 +23,50 @@ export const questions: Question[] = [
 ];
 
 function Faq() {
-    const [selectedQuestion, setSelectedQuestion] = useState("");
-    const [showAnswer, setShowAnswer] = useState(false);
-  
-    const handleQuestion = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        setShowAnswer(false);
-        const selectedQuestion = questions.find((q) => q.asking === e.currentTarget.value);
-        if (selectedQuestion) {
-            setSelectedQuestion(selectedQuestion.answer);
-            setShowAnswer(true);
-        }
-    };
-  
-    return (
-      <>
-        <section className="faqContainer">
-          <div className="faqTitle">
-            <h1>Frequently Asked Questions</h1>
-            <p>
-              Frequently Asked Questions About the Car Rental Booking Process on
-              Our Website: Answers to Common Concerns and Inquiries.
-            </p>
-          </div>
-          <div className="questionBox">
-            {questions.map((q) => (
-              <div key={q.asking}>
-                <button
-                  value={q.asking}
-                  onClick={handleQuestion}
-                  className="question"
-                >
-                  {q.asking}
-                  <FontAwesomeIcon icon={faChevronDown} className="arrow" />
-                </button>
-                {selectedQuestion === q.answer && (
-                  <p className={`answer ${showAnswer ? "show" : ""}`}>
-                  {selectedQuestion}
-                </p>
-                )}
-              </div>
-            ))}
-          </div>
-        </section>
-      </>
-    );
-  }
-  
+  const [selectedQuestion, setSelectedQuestion] = useState("");
+
+  const handleQuestion = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const selectedQuestion = questions.find((q) => q.asking === e.currentTarget.value);
+    if (selectedQuestion) {
+      setSelectedQuestion(selectedQuestion.answer);
+    } else {
+      setSelectedQuestion("");
+    }
+  };
+
+  return (
+    <section className="faq">
+      <div className="container">
+        <div className="faqTitle">
+          <h1>Frequently Asked Questions</h1>
+          <p>
+            Frequently Asked Questions About the Car Rental Booking Process on
+            Our Website: Answers to Common Concerns and Inquiries.
+          </p>
+        </div>
+        <div className="questionBox">
+          {questions.map((q) => (
+            <div key={q.asking}>
+              <button
+                value={q.asking}
+                onClick={handleQuestion}
+                className="question"
+              >
+                {q.asking}
+                <FontAwesomeIcon
+                  icon={faChevronDown}
+                  className="arrow"
+                />
+              </button>
+              {selectedQuestion === q.answer && (
+                <p className="answer">{selectedQuestion}</p>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
 export default Faq;
