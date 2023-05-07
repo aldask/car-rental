f2bd3190cf1b70306626790f342ce851922ae306
Aldas <80693733+aldask@users.noreply.github.com>
2023-05-07 17:47:11 +0300

    Footer layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,39 @@
+function Footer() {
+    return (
+      <>
+        <section className="footer">
+          <div className="container">
+            <div className="footerBox">
+                <ul className="column1">
+                    <li><h2>Car<span className="blue strong">O</span></h2></li>
+                    <li><p>We offer something nice</p></li>
+                    <li><a href="tel:+4733378901">+47 333 78 901</a></li>
+                    <li><a href = "mailto: aaa@aa.com">Send Email</a></li>
+                </ul>
+                <ul className="column2">
+                    <li><h2>Company</h2></li>
+                    <li><p>We offer something nice</p></li>
+                    <li><a href="tel:+4733378901">+47 333 78 901</a></li>
+                    <li><a href = "mailto: aaa@aa.com">Send Email</a></li>
+                </ul>
+                <ul className="column3">
+                    <li><h2>Working Hours</h2></li>
+                    <li><p>We offer something nice</p></li>
+                    <li><a href="tel:+4733378901">+47 333 78 901</a></li>
+                    <li><a href = "mailto: aaa@aa.com">Send Email</a></li>
+                </ul>
+                <ul className="column4">
+                    <li><h2>Subscription</h2></li>
+                    <li><p>We offer something nice</p></li>
+                    <li><a href="tel:+4733378901">+47 333 78 901</a></li>
+                    <li><a href = "mailto: aaa@aa.com">Send Email</a></li>
+                </ul>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  }
+
+  export default Footer;
+  
