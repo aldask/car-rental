f26111521ae87769bf305c51919cf8872142db11
Aldas <80693733+aldask@users.noreply.github.com>
2023-05-21 20:20:30 +0300

    Routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import "../src/styles/Global/global-style.css";
-import HomePage from './pages/HomePage';
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Vehicles from "./pages/Vehicles";
+import OurTeam from "./pages/OurTeam";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/vehicles' element={<Vehicles />} />
+        <Route path='/ourteam' element={<OurTeam />} />
+        <Route path='/signin' element={<SignIn />} />
+      </Routes>
     </>
   );
 }
---
+++ src/pages/OurTeam.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function OurTeam() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+}
+
+export default OurTeam;
