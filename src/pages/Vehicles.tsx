7815c0ddb820a76f06195f0fbfa9f9f61838a638
Aldas <80693733+aldask@users.noreply.github.com>
2023-05-21 23:12:58 +0300

    Our cars done

--- src/pages/Vehicles.tsx
+++ src/pages/Vehicles.tsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import VehiclesFleet from "../components/VehiclesFleet";
 
 function Vehicles() {
   return (
     <>
       <Navbar />
+      <VehiclesFleet />
       <Footer />
     </>
   );
